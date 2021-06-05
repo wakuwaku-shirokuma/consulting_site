d6a5e4d00e0e2038ca9822bcb0b1c8ea840c6c90
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-06-05 20:33:45 +0900

    rails webpacker:install:react でreactのインストール、管理画面側に「Hello React!」と出力させる（初めてのReact）

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,17 +1,5 @@
-//process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-//const environment = require('./environment')
+const environment = require('./environment')
 
-//module.exports = environment.toWebpackConfig()
-
-const { environment } = require('@rails/webpacker')
-// 以下追記
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
-)
-// ここまで
-module.exports = environment.toWebpackConfig();
+module.exports = environment.toWebpackConfig()
